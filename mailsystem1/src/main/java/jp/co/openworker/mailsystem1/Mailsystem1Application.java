99da21a66015d3eb050ca3d373e4c997e95f8d25
maki567 <maki.takuya@ccaweb.co.jp>
2021-01-19 09:32:09 +0900

    1

---
+++ mailsystem1/src/main/java/jp/co/openworker/mailsystem1/Mailsystem1Application.java
@@ -0,0 +1,13 @@
+package jp.co.openworker.mailsystem1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mailsystem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mailsystem1Application.class, args);
+	}
+
+}
---
+++ mailsystem1/src/test/java/jp/co/openworker/mailsystem1/Mailsystem1ApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.openworker.mailsystem1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mailsystem1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
