6f61ef31869f2176f7af963d52f550b212b28307
ok-com <nano123123123123@gmail.com>
2021-01-21 17:27:41 +0900

    login_pagecontroller、register_usrcontrollerの更新。

--- mailsystem1/src/main/java/jp/co/openworker/mailsystem1/controller/Login_PageController.java
+++ mailsystem1/src/main/java/jp/co/openworker/mailsystem1/controller/Login_PageController.java
@@ -1,5 +1,11 @@
 package jp.co.openworker.mailsystem1.controller;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
 public class Login_PageController {
-
+	  @RequestMapping("/login_page")
+	  public String index() {
+	    return "login_page/login_page";
+	  }
 }
--- mailsystem1/src/main/java/jp/co/openworker/mailsystem1/controller/Register_UserController.java
+++ mailsystem1/src/main/java/jp/co/openworker/mailsystem1/controller/Register_UserController.java
@@ -1,5 +1,11 @@
 package jp.co.openworker.mailsystem1.controller;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
 public class Register_UserController {
-
+	  @RequestMapping("/register_user")
+	  public String index() {
+	    return "register_user/register_user";
+	  }
 }
