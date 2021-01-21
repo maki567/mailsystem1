023930d24a81d6eea2d6fc8d029df39515f76f48
maki567 <maki.takuya@ccaweb.co.jp>
2021-01-21 10:06:47 +0900

    stsでhtmlの表示が可能になりました。

--- mailsystem1/src/main/java/jp/co/openworker/mailsystem1/controller/HomeController.java
+++ mailsystem1/src/main/java/jp/co/openworker/mailsystem1/controller/HomeController.java
@@ -1,17 +1,11 @@
 package jp.co.openworker.mailsystem1.controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
-@RequestMapping("/mailsystem/top_page")
 public class HomeController {
-
-  @RequestMapping("/")
-  public String index() {
-	  return "top_page";
-  //@ResponseBody
-  //public String index() {
-    //return "<a href=\"/logout\">ログアウト</a>";
-  }
+	  @RequestMapping("/home")
+	  public String index() {
+	    return "test1/test";
+	  }
 }
