79ce648b12d5d82ec53e6d4b8d73b6bd4809b372
ok-com <nano123123123123@gmail.com>
2021-01-22 16:57:01 +0900

    adminにUser.javaを作成。

---
+++ mailsystem1/src/main/java/jp/co/openworker/mailsystem1/model/domain/User.java
@@ -0,0 +1,13 @@
+package jp.co.openworker.mailsystem1.model.domain;
+
+public class User {
+
+	private long id;
+	
+	private String password;
+	
+	private String first_name;
+	
+	private String family_name;
+	
+}
