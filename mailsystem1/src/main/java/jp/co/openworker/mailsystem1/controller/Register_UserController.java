d7196e3c134b5d64efaf211a76783139113ab7c4
maki567 <maki.takuya@ccaweb.co.jp>
2021-01-25 15:14:09 +0900

    dialog3つ追加、register_user.htmlの51行目でエラー

--- mailsystem1/src/main/java/jp/co/openworker/mailsystem1/controller/Register_UserController.java
+++ mailsystem1/src/main/java/jp/co/openworker/mailsystem1/controller/Register_UserController.java
@@ -2,6 +2,7 @@ package jp.co.openworker.mailsystem1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 public class Register_UserController {
 	  @RequestMapping("/register_user")
